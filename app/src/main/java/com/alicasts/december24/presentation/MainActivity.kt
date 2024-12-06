ee7e1e37315b7a0e132584ed094d5e951123112f
Ali Win <ali.cast.santos@gmail.com>
2024-12-06 14:30:31 -0300

    chore: setup base package structure

--- app/src/main/java/com/alicasts/december24/presentation/MainActivity.kt
+++ app/src/main/java/com/alicasts/december24/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.alicasts.december24
+package com.alicasts.december24.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.alicasts.december24.ui.theme.December24Theme
+import com.alicasts.december24.presentation.theme.December24Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/alicasts/december24/presentation/theme/Theme.kt
+++ app/src/main/java/com/alicasts/december24/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.alicasts.december24.ui.theme
+package com.alicasts.december24.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
