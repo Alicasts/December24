6d37100da9765f63e5bb1942a079d7142961d14a
Ali Win <ali.cast.santos@gmail.com>
2024-12-11 18:12:04 -0300

    feat: implement filterById on RideHistoryResponseScreen

--- app/src/main/java/com/alicasts/december24/presentation/ride_history_response_screen/RideHistoryResponseViewModel.kt
+++ app/src/main/java/com/alicasts/december24/presentation/ride_history_response_screen/RideHistoryResponseViewModel.kt
@@ -4,19 +4,33 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.alicasts.december24.R
 import com.alicasts.december24.data.models.ride_history.Ride
 import com.alicasts.december24.data.repository.ride_history.interfaces.RideHistoryRepository
+import com.alicasts.december24.utils.Constants.VALID_DRIVERS
 import com.alicasts.december24.utils.Resource
+import com.alicasts.december24.utils.StringResourceProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RideHistoryResponseViewModel @Inject constructor(
-    private val repository: RideHistoryRepository,
+    stringResourceProvider: StringResourceProvider,
+    private val repository: RideHistoryRepository
 ) : ViewModel() {
     private val _rideHistory = MutableLiveData<Resource<List<Ride>>>()
     val rideHistory: LiveData<Resource<List<Ride>>> = _rideHistory
+
+    private val _filteredRides = MutableLiveData<List<Ride>>()
+    val filteredRides: LiveData<List<Ride>> = _filteredRides
+
+    private val allString = stringResourceProvider.getString(R.string.all)
+
+    private val _selectedDriverId = MutableLiveData(allString)
+    val selectedDriverId: LiveData<String> = _selectedDriverId
+
+    val driverIds = listOf(stringResourceProvider.getString(R.string.all)) + VALID_DRIVERS.map { it.driverId.toString() }
 
     fun fetchRideHistory(customerId: String, driverId: String) {
         viewModelScope.launch {
@@ -24,10 +38,25 @@ class RideHistoryResponseViewModel @Inject constructor(
             try {
                 val rides = repository.getRideHistory(customerId, driverId)
                 _rideHistory.value = Resource.Success(rides)
+                applyFilter(allString)
             } catch (e: Exception) {
                 val errorMessage = e.message
                 _rideHistory.value = errorMessage?.let { Resource.Error(it) }
             }
         }
+    }
+
+    fun applyFilter(selectedDriverId: String) {
+        val currentRides = (_rideHistory.value as? Resource.Success)?.data ?: emptyList()
+        _filteredRides.value = if (selectedDriverId == allString) {
+            currentRides
+        } else {
+            currentRides.filter { it.driver.id.toString() == selectedDriverId }
+        }
+    }
+
+    fun updateSelectedDriverId(driverId: String) {
+        _selectedDriverId.value = driverId
+        applyFilter(driverId)
     }
 }
--- app/src/test/java/com/alicasts/december24/presentation/ride_history_response_screen/RideHistoryResponseViewModelTest.kt
+++ app/src/test/java/com/alicasts/december24/presentation/ride_history_response_screen/RideHistoryResponseViewModelTest.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.Observer
 import com.alicasts.december24.data.models.shared.HistoryResponseDriver
 import com.alicasts.december24.data.models.ride_history.Ride
 import com.alicasts.december24.data.repository.ride_history.interfaces.RideHistoryRepository
+import com.alicasts.december24.presentation.mocks.FakeStringResourceProvider
 import com.alicasts.december24.utils.Resource
 import io.mockk.coEvery
 import io.mockk.mockk
+import io.mockk.verify
 import io.mockk.verifySequence
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -29,6 +31,7 @@ class RideHistoryResponseViewModelTest {
     private lateinit var viewModel: RideHistoryResponseViewModel
     private val repository = mockk<RideHistoryRepository>()
     private val testDispatcher = UnconfinedTestDispatcher()
+    private val stringResourceProvider = FakeStringResourceProvider()
 
     private val mockDriver = HistoryResponseDriver(id = 1, name = "John Doe")
     private val mockRides = listOf(
@@ -36,11 +39,12 @@ class RideHistoryResponseViewModelTest {
         Ride(2, "2023-12-08", "Origin2", "Destination2", 20.0, "25 min", mockDriver, 100.0)
     )
     private val errorMessage = "An error occurred"
+    private val allString = "Fake String"
 
     @Before
     fun setUp() {
         Dispatchers.setMain(testDispatcher)
-        viewModel = RideHistoryResponseViewModel(repository)
+        viewModel = RideHistoryResponseViewModel(stringResourceProvider, repository)
     }
 
     @After
@@ -49,6 +53,7 @@ class RideHistoryResponseViewModelTest {
     }
 
     private fun createObserver() = mockk<Observer<Resource<List<Ride>>>>(relaxed = true)
+    private fun createFilteredRidesObserver() = mockk<Observer<List<Ride>>>(relaxed = true)
 
     @Test
     fun `fetchRideHistory should post Loading and then Success`() = runTest {
@@ -75,6 +80,49 @@ class RideHistoryResponseViewModelTest {
         verifySequence {
             observer.onChanged(match { it is Resource.Loading })
             observer.onChanged(match { it is Resource.Error && it.message == errorMessage })
+        }
+    }
+
+    @Test
+    fun `applyFilter with All should post all rides`() = runTest {
+        coEvery { repository.getRideHistory(any(), any()) } returns mockRides
+        val filteredObserver = createFilteredRidesObserver()
+        viewModel.filteredRides.observeForever(filteredObserver)
+
+        viewModel.fetchRideHistory("customerId", "driverId")
+        viewModel.applyFilter(allString)
+
+        verify {
+            filteredObserver.onChanged(mockRides)
+        }
+    }
+
+    @Test
+    fun `applyFilter with specific driverId should post filtered rides`() = runTest {
+        coEvery { repository.getRideHistory(any(), any()) } returns mockRides
+        val filteredObserver = createFilteredRidesObserver()
+        viewModel.filteredRides.observeForever(filteredObserver)
+
+        viewModel.fetchRideHistory("customerId", "driverId")
+        viewModel.applyFilter("1") // Filter by driverId = 1
+
+        verify {
+            filteredObserver.onChanged(mockRides.filter { it.driver.id.toString() == "1" })
+        }
+    }
+
+    @Test
+    fun `updateSelectedDriverId should update selectedDriverId and apply filter`() = runTest {
+        coEvery { repository.getRideHistory(any(), any()) } returns mockRides
+        val filteredObserver = createFilteredRidesObserver()
+        viewModel.filteredRides.observeForever(filteredObserver)
+
+        viewModel.fetchRideHistory("customerId", "driverId")
+        viewModel.updateSelectedDriverId("1")
+
+        assert(viewModel.selectedDriverId.value == "1")
+        verify {
+            filteredObserver.onChanged(mockRides.filter { it.driver.id.toString() == "1" })
         }
     }
 }
